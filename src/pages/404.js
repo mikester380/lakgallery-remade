91b28508666c26692cf7458f74d29c89552d210c
mikestaarr <mhykhelofficial@gmail.com>
2023-01-04 19:49:54 +0100

    edited error page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,26 +1,23 @@
 import React from "react";
-import Layout from "../components/Layout";
 
 const ErrorPage = () => {
   return (
-    <Layout>
-      <div className="error">
-        <div className="error__top">
-          <h2 className="error__title">Page Not Found</h2>
+    <div className="error">
+      <div className="error__top">
+        <h2 className="error__title">Page Not Found</h2>
+      </div>
+      <div className="error__bottom">
+        <div className="error__bottom-left">
+          <p>(404)</p>
         </div>
-        <div className="error__bottom">
-          <div className="error__bottom-left">
-            <p>(404)</p>
-          </div>
-          <div className="error__bottom-right">
-            <p>
-              Unfortunately, we were not able to locate the page that you are
-              trying to reach.
-            </p>
-          </div>
+        <div className="error__bottom-right">
+          <p>
+            Unfortunately, we were not able to locate the page that you are
+            trying to reach.
+          </p>
         </div>
       </div>
-    </Layout>
+    </div>
   );
 };
 
